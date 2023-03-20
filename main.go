47dc8c96b28cc054f070c2d02f18d74f0ebecbaf
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-03-20 16:36:01 +0100

    handling error

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -12,6 +13,23 @@ const portNUmber = ":8080"
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "this is my Home page")
 
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "canot divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+}
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New(("cannot divie by zero"))
+		return 0, err
+	}
+	result := x / y
+	return result, nil
 }
 
 // About is the about page handler
@@ -26,6 +44,7 @@ func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/about", Divide)
 	fmt.Println(fmt.Sprintf("Starting ap on port %s", portNUmber))
 	http.ListenAndServe(portNUmber, nil)
 }
